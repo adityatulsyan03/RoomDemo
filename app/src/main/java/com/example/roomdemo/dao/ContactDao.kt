e8339518b0307b43b019aff4a52cc5c96836e1e0
adityatulsyan03 <tulsyanaditya03@gmail.com>
2025-02-19 03:47:30 +0530

    Initial Commit

--- app/src/main/java/com/example/roomdemo/dao/ContactDao.kt
+++ app/src/main/java/com/example/roomdemo/dao/ContactDao.kt
@@ -11,6 +11,7 @@ interface ContactDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertContact(contact: Contact): Long
 
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertContactAge(contactAge: ContactAge)
 
